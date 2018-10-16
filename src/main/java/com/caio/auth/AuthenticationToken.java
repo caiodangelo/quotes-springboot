53374d1281eab35ca12c4d4c0f2398c4730a055a
Stefan Erichsen <stefan@balticfinance.com>
2018-10-16 22:39:40 +0200

    Implements Header authentication #2

---
+++ src/main/java/com/caio/auth/AuthenticationToken.java
@@ -0,0 +1,46 @@
+package com.caio.auth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class AuthenticationToken implements Authentication {
+
+    private boolean authenticated;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return this.authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
